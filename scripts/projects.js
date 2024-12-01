dd77d3c9d6a54dc2657cdce58c8b5b0772005ec2
kai <kaialbertson@gmail.com>
2024-12-01 02:11:43 -0800

    added project viewer

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,17 +1,31 @@
-const Project = ({ title, languages, date }) => `
-<div class="project-box cursor-link">
+const Project = ({ title, languages, date, id }) => `
+<div class="project-box cursor-link" onclick="open_project('${id}');">
     <p class="project-title">${title}</p>
     <p class="project-langs">${languages}</p>
     <p class="project-date">${date}</p>
 </div>
 `;
 
+async function open_project(id){
+    var response = await fetch("projects/"+id+"/page.md");
+    var text = await response.text();
+    $("#project-view")[0].innerHTML = marked.parse(text);
+    $("#project-view").addClass("open");
+    $("#project-view-block").addClass("open");
+}
+
+function close_project(){
+    $("#project-view").removeClass("open");
+    $("#project-view-block").removeClass("open");
+}
+
 function add_project(project, parent){
     var element = document.createElement('div');
     element.innerHTML = Project({
         "title": project.title,
         "languages": project.langs,
-        "date": project.date
+        "date": project.date,
+        "id": project.id
     });
 
     parent.appendChild(element.children[0]);
